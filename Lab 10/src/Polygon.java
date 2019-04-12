c7a4cc41d4c9a91b425eaf6db4598289301f9e19
Nick Njoku <nnjoku245@ou.edu>
2019-04-12 15:38:37 -0500

    Started to create the shape classes that were not provided.
    Not working on actual methods in classes yet

---
+++ Lab 10/src/Polygon.java
@@ -0,0 +1,32 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+
+public abstract class Polygon extends Shape {
+	
+	public Polygon(Color color, boolean filled)
+	{
+		super(color, filled);
+	}
+	
+	public void draw(Graphics graphics)
+	{
+		
+	}
+	
+	public Color getColor()
+	{
+		
+	}
+	
+	public boolean isFilled()
+	{
+		
+	}
+	
+	public Point[] getLocation()
+	{
+		
+	}
+
+}
